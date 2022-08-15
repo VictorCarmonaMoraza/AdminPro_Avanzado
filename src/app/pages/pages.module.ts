7b190c716874be5d39896268bbdd37b6d5f2d6b1
Victor <VictorCarmona9035@hotmail.com>
2022-08-15 19:23:54 +0200

    15-Uso de atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
+import {FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,8 +17,9 @@ import { ProgressComponent } from './progress/progress.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
   ],
   exports:[
     ProgressComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -5,11 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  progreso: number = 50;
 
-  ngOnInit(): void {
+  //Retornamos el porcentaje del progreso
+  get getPorcentaje() {
+    return `${this.progreso}%`
+  }
+
+  //Metod para cambiar valor del progreso
+  cambiarValor(valor: number) {
+
+    if (this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100;
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+
+    return this.progreso = this.progreso + valor;
   }
 
 }
