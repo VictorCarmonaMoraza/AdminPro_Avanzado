20b9de38bd7bd57608e2eed6167be36f5a14ec43
Victor <VictorCarmona9035@hotmail.com>
2022-08-15 09:41:46 +0200

    8-Pagina 404

--- src/app/pages/nopagesfound/nopagesfound.component.ts
+++ src/app/pages/nopagesfound/nopagesfound.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NopagesfoundComponent implements OnInit {
 
+
+  year = new Date().getFullYear();
+  
   constructor() { }
 
   ngOnInit(): void {
