7f09abf3de4bed97d19ced0a5a8cb5ef235d7fac
Victor <VictorCarmona9035@hotmail.com>
2022-08-27 12:55:16 +0200

    52-Pulir detalles de nuestro incrementador component

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -7,16 +7,15 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class IncrementadorComponent implements OnInit {
 
-  ngOnInit(){
-    this.btnClass =`btn ${this.btnClass}`
-  }
-
-
 
   @Input() progreso: number = 40;
-  @Input() btnClass:string='btn-primary';
+  @Input() btnClass: string = 'btn-primary';
 
-  @Output() valorSalida:EventEmitter<number>=new EventEmitter<number>()
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter<number>();
+
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`
+  }
 
   //Metod para cambiar valor del progreso
   cambiarValor(valor: number) {
@@ -33,5 +32,19 @@ export class IncrementadorComponent implements OnInit {
 
     this.progreso = this.progreso + valor;
     return this.valorSalida.emit(this.progreso);
+  }
+
+  //Metodo para cuando cambia el valor del input 
+  onChange(newValor: number) {
+
+    if (newValor >= 100) {
+      this.progreso = 100;
+    } else if (newValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = newValor;
+    }
+    //Cuando el valor cambia quiero emitir
+    this.valorSalida.emit(this.progreso);
   }
 }
