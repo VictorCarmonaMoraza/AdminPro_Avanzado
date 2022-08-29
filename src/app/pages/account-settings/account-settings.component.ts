3a6e7f1ad7ce105fd4306eed965c2475330907b5
Victor <VictorCarmona9035@hotmail.com>
2022-08-29 19:49:47 +0200

    25-Agregando clases de CSS sin usar ngClass

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,25 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 
 
-  linkTheme = document.querySelector('#theme');
+  public linkTheme = document.querySelector('#theme');
+  //Obtenemos todos los elementos con la clase selector
+  public links!:NodeListOf<Element>;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
   changeTheme(theme: string) {
 
-
     const url = `./assets/css/colors/${theme}.css`;
 
     //Esto es de vanilla jascript, sustituye el atributo href por el que tenemos
-    this.linkTheme?.setAttribute('href', url);
+    this.linkTheme?.setAttribute('href',url);
 
     //Para que cuando recarguemos no nos modifique el tema que habiamos seleccionado lo guardemos el localstorage
-    localStorage.setItem('theme', url);
+    localStorage.setItem('theme',url);
 
-    console.log(url);
+    this.checkCurrentTheme();
+    //console.log(url);
+  }
+
+  checkCurrentTheme():void{
+    //Recorremos todos los elementos html
+    this.links.forEach(elem=>{
+      //Borramos la clase working de los elementos
+      elem.classList.remove('working');
+      //Obtengo el tema seleccionado mediante su atributo
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl=`./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if(btnThemeUrl===currentTheme){
+        elem.classList.add('working');
+      }
+    });
+    //console.log(links);
+
   }
 
 }
