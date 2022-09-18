e3b40d06f46dbe22577428c092996ccbcba858a5
Victor <VictorCarmona9035@hotmail.com>
2022-09-18 17:28:56 +0200

    83-Llamar el unsubscribe

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,34 +1,23 @@
-import { Component } from '@angular/core';
-import { filter, interval, map, Observable, retry, take } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { filter, interval, map, Observable, retry, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+
+  public intervalSubs: Subscription;
 
   constructor() {
 
-    //Nos suscribimos al observable
-    // this.retornaObservable().pipe(
-    //   retry(1)
-    // )
-    //   .subscribe(
-    //     valor => console.log('Subs:', valor),
-    //     error => console.warn('Error:', error),
-    //     () => console.info('Obs terminado')
-    //   );
-
-    //Opcion 1
-    // this.retornaIntervalo()
-    //   .subscribe(
-    //     (valor) => console.log(valor)
-    //   )
-
-    //Opcion 2
-    this.retornaIntervalo()
+    this.intervalSubs = this.retornaIntervalo()
       .subscribe(console.log)
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
   }
 
   retornaIntervalo(): Observable<number> {
@@ -37,7 +26,7 @@ export class RxjsComponent {
       .pipe(
         map(valor => valor + 1),
         filter(valor => (valor % 2 === 0) ? true : false),
-        take(10),
+        //take(10),
       )
       ;
   }
