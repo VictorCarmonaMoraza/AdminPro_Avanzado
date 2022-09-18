f65b30f07ce50d4eb4cac52cfdb755056fdf7f60
Victor <VictorCarmona9035@hotmail.com>
2022-09-18 12:26:40 +0200

    77-Crear un observable manualmente

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor() {
+    //Creacion de un observable
+    const obs$ = new Observable(observer => {
+      let i = -1;
+
+      const intervalo = setInterval(() => {
+        i++;
+        //Next es para emitir
+        observer.next(i);
+        //Cancelamos el observer cuando i ==4
+        if (i == 4) {
+          clearInterval(intervalo);
+          //Notificar que ya se cancelo
+          observer.complete();
+        }
+        if (i == 2) {
+          observer.error('i llego al valor de 2');
+        }
+        // console.log('tick');
+      }, 1000)
+    });
+
+    //Nos suscribimos al observable
+    obs$.subscribe(
+      valor => console.log('Subs:', valor),
+      error => console.warn('Error:', error),
+      () => console.info('Obs terminado')
+    );
   }
 
 }
